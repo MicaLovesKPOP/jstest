7cdf2f1cfd6c29c484c6f9e692b5eea3c08584e0
MicaLovesKPOP <25252229+MicaLovesKPOP@users.noreply.github.com>
2023-10-10 23:17:40 +0200

    auto commit

--- script.js
+++ script.js
@@ -12,35 +12,4 @@ svgImage.src = `https://micaloveskpop.github.io/jstest/vehicles/sedan_001/sprite
 svgImage.onload = function () {
     // Once the SVG is loaded, draw it on the canvas
     ctx.drawImage(svgImage, 0, 0);
-
-    // Your game logic here:
-    // - Initialize variables
-    // - Add event listeners for game controls
-    // - Implement game loop (requestAnimationFrame)
-    // - Add functions for updating and rendering the game
-
-    // Example:
-    let carX = 100;
-    let carY = 100;
-    
-    function updateGameArea() {
-        // Update game logic
-    }
-    
-    function drawGame() {
-        // Clear canvas
-        ctx.clearRect(0, 0, canvas.width, canvas.height);
-        
-        // Draw the car or other game elements
-        ctx.drawImage(svgImage, carX, carY);
-    }
-    
-    function gameLoop() {
-        updateGameArea();
-        drawGame();
-        requestAnimationFrame(gameLoop);
-    }
-
-    // Start the game loop
-    gameLoop();
 };
