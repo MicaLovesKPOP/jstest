36c17d2a6cc054096de22199fd73e7eef8b82074
MicaLovesKPOP <25252229+MicaLovesKPOP@users.noreply.github.com>
2023-10-10 21:29:31 +0200

    auto commit

--- script.js
+++ script.js
@@ -199,7 +199,7 @@ function drawCar() {
   
   // Draw a red rectangle behind the car image
   ctx.fillStyle = "red";
-  ctx.fillRect(scaledCarX, scaledCarY, carWidth, carHeight);
+  ctx.fillRect(scaledCarX, scaledCarY, 100, 100);
 
   // Draw the car image on top of the red rectangle
   ctx.drawImage(carImage, scaledCarX, scaledCarY, carWidth, carHeight);
